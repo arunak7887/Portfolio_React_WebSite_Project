823edda33ae1c912262c26386f4ffe0970e01168
arunak7887 <arunkinwad02@gmail>
2024-02-19 09:08:28 +0530

    first commit

---
+++ react-developer-portfolio-template-main/src/Details.js
@@ -0,0 +1,174 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/Mysql.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/Java.png";
+import tailwind from "./assets/techstack/Spring_Boot.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/Eclipse.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/Intellji.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Arun Kinwad",
+  tagline: "I am a Java developer, Backend & front-end web developer,Salesforce  developer",
+  img: profile,
+  about: ` Work Experience in Java,AdvanceJava, Mysql,Javascript,Spring,Spring Boot,ReactJs,Hibernate | Knowledge and experience in OOPS,Web Development,Front End Technologies,Database Mysql and Debugging |open to work for Software Engineer,Front End Developer ,Salesforce Developer.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/arun-kinwad-a523b91b0/",
+  github: "https://github.com/arunak7887/",
+  twitter: "https://twitter.com/kinwad15416",
+  instagram: "https://www.instagram.com/arun_kinwad/",
+};
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Java Full Stack Developer Course",
+    Company: "Profound Edutech",
+    Location: "Karve Nagar Pune ",
+    Type: "Full Time",
+    Duration: "Nov 2023 - Present",
+  },
+  {
+    Position: "Bachelor in Information Technology",
+    Company: `D.Y. Patil College of Engineering, Akurdi, Pune`,
+    Location: "Pune,Maharashtra,India",
+    Type: "Full Time",
+    Duration: "Aug 2029 - June 2023",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Project title 1",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description A todo application is a digital tool that helps users organize tasks, set priorities, and manage their to-do lists efficiently. It enables users to track deadlines, categorize tasks, and improve productivity by providing a centralized platform for task management.`,
+    techstack: "Java,Spring Boot,HTML/CSS,Javascript",
+    previewLink: "https://arunak7887.github.io/ToDoList_SpringBoot_Project/",
+    githubLink: "https://github.com/arunak7887/ToDoList_SpringBoot_Project",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "arunkinwad02@gmail.com",
+  phone: "+91 7977282013 ||  7588578169",
+};
